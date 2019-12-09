1c5943cf78f1f960046f51587e991232856a41a1
Malik-hamza29 <58634310+Malik-hamza29@users.noreply.github.com>
2019-12-09 23:59:57 +0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//#include<conio.h>
+main()
+{
+    int a,b,c;
+   //clrscr();
+    printf("Enter number 1\n");
+    scanf("%d",&a);
+    printf("Enter number 2\n");
+    scanf("%d",&b);
+    c=a-b;
+    printf("subbtraction is     :%d\n", c);
+  // getch();
+}
